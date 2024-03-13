24e9ae0bd5ec4fac103a709d44a8b1fbd9dc3080
RyanBellNEIT <rbell@email.neit.edu>
2024-03-12 20:09:20 -0400

    Added descriptions to projects.

--- scripts/projectPageScript.js
+++ scripts/projectPageScript.js
@@ -1,7 +1,7 @@
 const projectMap = {
-    lidarVR: ["Lidar VR", "description"],
-    punchBack: ["Punch Back", "description"],
-    ristrovikCrisis: ["Ristrovik Crisis", "description"]
+    lidarVR: ["Lidar VR", "LiDAR VR is a unique 3D VR horror game built in Unity Engine C#. You take the role of a scientist entering a simulation where the world can only be drawn by using your LiDAR scanner. After getting stuck inside of the simulation, you must traverse your way out of the world and find your freedom among the simulation. However, you are not alone as the thing that trapped you also wants you dead."],
+    punchBack: ["Punch Back", "Punch Back is a Fast-paced 2D Platformer built in Unity Engine and C#. In Punch Back, you play as a lumberjack on his daily shift in the winter. Unfortunately during your shift, the birds and the snow men have teamed up to steal your axe for good and rid you from doing your job. It is up to you to retrive your axe back from the birds. Use your first to traverse the map in this 2D fast paced platformer."],
+    ristrovikCrisis: ["The Ristrovik Crisis", "The Ristrovik Crisis is a 3D horror game taking place in the 80s. You are a liquidator being sent in to stop the disaster taking place at a nuclear facility that has melted down. Your job is to prevent radiation from spreading outside the country and causing mass destruction on the surrounding area. You are not alone in the facility however, the radiation has caused some unforeseen cirumstances among the facility grounds."]
 }
 
 let params = new URL(document.location).searchParams;
